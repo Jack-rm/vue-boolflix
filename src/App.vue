1b3a587aa3c78abdb4846d0a2d146fc6255210e8
Jack-rm <jackcnsr94@gmail.com>
2021-10-01 17:38:10 +0200

    Setting up axios call

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a href="https://fontmeme.com/netflix-font/"><img src="https://fontmeme.com/permalink/211001/4937b74c6317b36c509baa13669b4b20.png" alt="netflix-font" border="0"></a>
+    <HelloWorld msg="Welcome to my Boolflix!"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
