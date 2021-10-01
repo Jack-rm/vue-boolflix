6629cf45f2ec84309f0ae94a7c8a7f7a4cce9512
Jack-rm <jackcnsr94@gmail.com>
2021-10-01 19:15:35 +0200

    hotfix Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <a href="https://fontmeme.com/netflix-font/"><img src="https://fontmeme.com/permalink/211001/4937b74c6317b36c509baa13669b4b20.png" alt="netflix-font" border="0"></a>
+        <img src="https://fontmeme.com/permalink/211001/4937b74c6317b36c509baa13669b4b20.png" alt="netflix-font" border="0">
         <br>
         <input type="text" placeholder="Movies" v-model="search" @keyup="$emit('upSearch', search)">    
     </div>
